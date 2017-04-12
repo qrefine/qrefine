2a202522e09f66757244f6efa332f6ff0a97693e
Mark Waller <mpwaller@users.noreply.github.com>
2017-04-12 13:38:52 +0800

    Update patch.sh

--- patch.sh
+++ patch.sh
@@ -1,4 +1,6 @@
 # A simple way to patch qrefine into PHENIX
+git clone https://github.com/qrefine/qr-core.git
+mv qr-core core
 phenix.python -m pip install -r requirements.txt
-#phenix.python -m pip install -r core/requirements.txt
+phenix.python -m pip install -r core/requirements.txt
 libtbx.configure qrefine
