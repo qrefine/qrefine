1866db1f5664ddcc2614b292b6d1a5320499ff3a
Mark Waller <mpwaller@users.noreply.github.com>
2017-04-20 07:09:38 +0800

    Update refine.py

--- command_line/refine.py
+++ command_line/refine.py
@@ -20,15 +20,7 @@ example_path = os.path.join(qrefine_path,"examples")
 log = sys.stdout
 
 def help():
-  """ Commands in qrefine:
-      - start (including restart, check{dry run, and 1SCF, qmready} )
-      - pause
-      - stop
-      - show
-      - help
-      - test
-      - example
- 
+  """  
   Options and keywords in qrefine:
        - qm_calculator
        - macro_cycles
@@ -42,25 +34,20 @@ def help():
    """
 
 def stop(args, log):
+  """how do we get a reference to all of the running jobs.
+  This could be hundreds of qdels, must automate this."""
   jobid = args[1]
   print "Are you sure you want to stop job {}".format(jobid)
-  # how do we get a reference to all of the running jobs.
-  # This could be hundreds of qdels, must automate this.
-
 
 def pause():
   id = 0
   print "Are you sure you want to pause job {}".format(id)
-  t0 = time.time()
-  log = sys.stdout
-  #qr.run(args=sys.argv[1:], log=log)
-  cmds= []
   # example job ids, just for getting the code to work
   sub_job_ids =['305.mu01','306.mu01']
+  cmds= []
   for sub_job_id in sub_job_ids:
     cmds.append(" qhold" +  sub_job_id)
   easy_run.call(cmds)
-  print >> log, "Time: %6.4f" % (time.time() - t0)
 
 def run_cmd(cmd):
   # we want a reference to the running job.
@@ -79,21 +66,9 @@ def example():
       os.path.join(qrefine_core_path,"qr.py ") +  \
       os.path.join(qrefine_path,"examples/1us0/data.mtz ") + \
       os.path.join(qrefine_path,"examples/1us0/a87_99_h.pdb ") + \
-       """max_iterations = 90
-          max_atoms = 10000
-          number_of_micro_cycles = 20
-          qm_calculator = terachem
-          mode = refine
-          stpmax = 0.9
-          restraints = qm
-          gradient_only = true
-          line_search = true
-          shake_sites = false
-          output_folder_name = refine
-          restraints_weight_scale = 32
+       """output_folder_name = refine
           > refine.log"""
   run_cmd(cmd)
-
 
 def run(args, log):
   cmd = " phenix.python " +  \
