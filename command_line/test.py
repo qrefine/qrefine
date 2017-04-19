b62331088926a3f4e0aaf5c45b3a00f5a8be299c
Mark Waller <mpwaller@users.noreply.github.com>
2017-04-19 20:39:43 +0800

    Update test.py

--- command_line/test.py
+++ command_line/test.py
@@ -2,17 +2,18 @@ from __future__ import division
 # LIBTBX_SET_DISPATCHER_NAME qr.test
 import sys
 import argparse
-from qrefine.tests      import regression_tests
+from qrefine.tests.regression import test_reg_04_cluster
+#from qrefine.tests      import regression_tests
 from qrefine.core.tests import run_tests as unit_tests
 
 if __name__ == '__main__':
   print " Testing Quantum Refinement Program"
   parser = argparse.ArgumentParser(description='Test runners for Q|R code')
   parser.add_argument('--unit',       action='store_true',
-                                      default=True,
+                                      default=False,
                                       help='run unit tests ')
   parser.add_argument('--regression', action='store_true',
-                                      default=False,
+                                      default=True,
                                       help='run regression tests, will take a long time') # nightly build?
   parser.add_argument('--pdb',        action='store_true',
                                       default=False,
@@ -20,6 +21,8 @@ if __name__ == '__main__':
                                               only run on a HPC cluster''')
 
   args = parser.parse_args()
-  if (args.unit)      : unit_tests.run()
-  if (args.regression): regression_tests.run(args=sys.argv[1:])
+  if (args.unit)      :  unit_tests.run()
+  if (args.regression):
+    test_reg_04_cluster.run("/home/xuyanting/phenix/phenix-1.11.1-2575/modules/qrefine/core/tests/data_files/2lvr.pdb")
+   #regression_tests.run(args=sys.argv[1:])
   if (args.pdb)       : regression_tests.run(args=sys.argv[1:])
