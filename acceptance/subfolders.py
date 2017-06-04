3d31df1edfe7cb01ba4d0a90f56f23155640fb36
Mark Waller <mpwaller@users.noreply.github.com>
2017-06-04 11:51:10 +0800

    Update subfolders.py

--- acceptance/subfolders.py
+++ acceptance/subfolders.py
@@ -7,9 +7,9 @@ new_dir="/home/xuyanting/results_new"
 files_per_folder=1000
 
 file_names=[]
-for file in listdir(root_dir):
+for c in listdir(root_dir):
     #make a new sub dir for every 1000 files 
-    file_names.append(file)
+    file_names.append(input_file)
 print len(file_names)
 
 folders = []
