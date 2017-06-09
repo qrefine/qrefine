92352cb1dcf59fe3a36071c10171159e8471989d
Nigel W. Moriarty <NWMoriarty@LBL.Gov>
2017-06-09 00:28:38 -0700

    nice messages

--- command_line/build_interfaces.py
+++ command_line/build_interfaces.py
@@ -3,12 +3,7 @@ import os, sys
 
 from libtbx import easy_run
 
-def run():
-  width=78
-  msg = ['',
-         'Welcome to the Q|R inteface checker',
-         '',
-         ]
+def draw_box_around_text(msg, width=78):
   print '\n%s' % ('%s%s%s' % (u'\u250C',
                               u'\u2500'*width,
                               u'\u2510',
@@ -26,6 +21,13 @@ def run():
                             u'\u2518',
                            )
                 )
+
+def run():
+  msg = ['',
+         'Welcome to the Q|R inteface checker',
+         '',
+         ]
+  draw_box_around_text(msg, width=78)
 
   cmd = 'java -version'
   rc = easy_run.go(cmd)
@@ -61,22 +63,34 @@ def run():
 
   qm_engine_env_vars = {'MOPAC_CMD' : 'Mopac executable',
                         }
-  count = 0
+  count = []
   for env_var in qm_engine_env_vars:
     if os.environ.get(env_var, False):
-      print '\n  Environmental variable %s set for "%s" to %s\n' % (
+      print '\n  Environmental variable %s set for "%s" to "%s"\n' % (
         env_var,
         qm_engine_env_vars[env_var],
         os.environ[env_var],
         )
-      count+=1
+      if not os.path.exists(os.environ[env_var]):
+        print '''
+        Environmental variable "%s" : "%s" does not point to anything!
+
+        STOPPING
+        ''' % (env_var, os.environ[env_var])
+        sys.exit()
+      count.append(env_var)
     else:
       print '\n  Environmental variable %s for "%s" not found\n' % (
         env_var,
         qm_engine_env_vars[env_var],
         )
   if count:
-    pass
+    print '''
+    QM engines set
+    '''
+    for env_var in count:
+      print '%s %s : %s' % (' '*10, env_var, os.environ[env_var])
+    print
   else:
     print '''
     No QM engines found!
@@ -86,6 +100,8 @@ def run():
     for env_var, help in qm_engine_env_vars.items():
       print '%s %s : %s' % (' '*10, env_var, help)
     print
+
+  draw_box_around_text(['','Done!','','Run a Q|R job using qr.refine',''])
 
 if __name__=="__main__":
   args = sys.argv[1:]
