53695c808802c3d8948f0c14ae55aad002e570c4
mpwaller@protonmail.com <mpwaller@protonmail.com>
2019-02-23 09:14:53 +1100

    added unit test for torchani

---
+++ tests/unit/tst_31.py
@@ -0,0 +1,55 @@
+import run_tests
+from ase import Atoms
+from qrefine.plugin.ase.torchani_qr import TorchAni
+
+try:
+    import torch
+    import torchnani
+    torchani_installed = True
+except ImportError:
+    torchani_installed = False
+
+device = torch.device('cpu')
+
+def run(prefix):
+    """
+    Exercise TorchANI calculator works as expected.
+    :param prefix:
+    :return:
+    """
+    calculator = TorchAni(label="ase",atoms=None, )
+
+    atoms = Atoms('CHHHH',[[0.03192167, 0.00638559, 0.01301679],
+                                 [-0.83140486, 0.39370209, -0.26395324],
+                                 [-0.66518241, -0.84461308, 0.20759389],
+                                 [0.45554739, 0.54289633, 0.81170881],
+                                 [0.66091919, -0.16799635, -0.91037834]])
+
+    print "atoms", atoms
+    print "coordinate", atoms.get_positions()
+
+    calculator.run_qr(atoms=atoms,coordinates=atoms.get_positions(),charge=0,pointcharges=None )
+
+    print "energy is :", calculator.energy_free
+    print "force is :", calculator.forces
+
+
+    assert calculator.energy_free == -40.4244117737
+    assert calculator.forces == [[-0.00555373, -0.00059946,  0.00076646],
+ [ 0.01750624, -0.01134875,  0.00614653],
+ [ 0.00449754,  0.01362024, -0.00396855],
+ [-0.00801547, -0.00424718, -0.00878889],
+ [-0.00843458,  0.00257515,  0.00584446]]
+    pass
+
+
+
+if(__name__ == "__main__"):
+  prefix="tst_31"
+  if torchani_installed:
+      print "disable"
+      rc = run_tests.runner(function=run, prefix=prefix, disable=True)
+  else:
+      print "enable"
+      rc = run_tests.runner(function=run, prefix=prefix, disable=False)
+  assert not rc, '%s rc: %s' % (prefix, rc)
