64d1bb9ce3ce94ee767e701746ad3052f67d6535
mpwaller@protonmail.com <mpwaller@protonmail.com>
2019-03-03 11:16:02 +1100

    udpated tst_31.py to include both models available in torchani

--- tests/unit/tst_31.py
+++ tests/unit/tst_31.py
@@ -2,10 +2,11 @@ import run_tests
 from ase import Atoms
 from qrefine.plugin.ase.torchani_qr import TorchAni
 import warnings
+from libtbx.test_utils import approx_equal
 
 def run(prefix):
     """
-    Exercise TorchANI calculator works as expected.
+    Exercise TorchANI calculator works as expected for both ANI1x and ANI1ccx
     """
 
     atoms = Atoms('CHHHH', [[0.03192167, 0.00638559, 0.01301679],
@@ -14,23 +15,34 @@ def run(prefix):
                             [0.45554739, 0.54289633, 0.81170881],
                             [0.66091919, -0.16799635, -0.91037834]])
 
-    calculator = TorchAni(method='ani-1x_8x',label="ase",atoms=atoms )
+    #Test the ANIx model
+    calculatorANIx = TorchAni(method='ani-1x',label="ase",atoms=atoms )
 
-    print "atoms", atoms
-    print "coordinate", atoms.get_positions()
+    calculatorANIx.run_qr(atoms=atoms,coordinates=atoms.get_positions(),charge=0,pointcharges=None )
 
-    calculator.run_qr(atoms=atoms,coordinates=atoms.get_positions(),charge=0,pointcharges=None )
+    assert calculatorANIx.method == 'ani-1x'
+    assert approx_equal(calculatorANIx.energy_free,  -40.459022522)
+    assert approx_equal(calculatorANIx.forces , [[ 0.0306, -0.1316, -0.0527],
+        [-0.1293,  0.1639, -0.0774],
+        [ 0.0856, -0.0429,  0.0408],
+        [ 0.0268,  0.0060,  0.0381],
+        [-0.0138,  0.0046,  0.0511]],0.001)
 
-    print "energy is :", calculator.energy_free
-    print "forces are :", calculator.forces
+    #Test the ANIccx model
 
-    assert calculator.method == 'ani-1x_8x'
-    assert calculator.energy_free == -40.425621032714844
-    assert calculator.forces == [[-0.00555373, -0.00059946,  0.00076646],
-                                [ 0.01750624, -0.01134875,  0.00614653],
-                                [ 0.00449754,  0.01362024, -0.00396855],
-                                [-0.00801547, -0.00424718, -0.00878889],
-                                [-0.00843458,  0.00257515,  0.00584446]]
+    calculatorANIccx = TorchAni(method='ani-1ccx', label="ase", atoms=atoms)
+
+    calculatorANIccx.run_qr(atoms=atoms, coordinates=atoms.get_positions(), charge=0, pointcharges=None)
+
+    assert calculatorANIccx.method == 'ani-1ccx'
+    assert approx_equal(calculatorANIccx.energy_free, -40.4256210327)
+    assert approx_equal(calculatorANIccx.forces, [[0.0312, -0.1272, -0.0511],
+                                                [-0.1200, 0.1628, -0.0761],
+                                                [0.0856, -0.0448, 0.0407],
+                                                [0.0219, 0.0044, 0.0343],
+                                                [-0.0187, 0.0049, 0.0521]], 0.001)
+
+
 
 if(__name__ == "__main__"):
   torchani_installed = False
