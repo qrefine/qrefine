c8d6039c9d8e141e786ff951d49cf085fb412726
Yanting Xu <1303697177@qq.com>
2019-04-27 22:14:21 +0800

    fix qr.finalise error for ligand
    Number of atoms with unknown nonbonded energy type symbols

--- finalise.py
+++ finalise.py
@@ -172,7 +172,7 @@ def run(pdb_filename,
   sel = asc.selection("element H or element D")
   model = mmtbx.model.manager(
     model_input               = None,
-    build_grm                 = True,
+    build_grm                 = False,
     pdb_hierarchy             = ppf.all_chain_proxies.pdb_hierarchy,
     pdb_interpretation_params = params,
     crystal_symmetry          = ppf.all_chain_proxies.pdb_inp.crystal_symmetry(),
