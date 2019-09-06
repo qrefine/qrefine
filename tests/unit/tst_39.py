c95c14cff9035708cbc55909dcb72936a205d372
Pavel <pafonine@lbl.gov>
2019-09-05 17:12:50 -0700

    Remove extra empty lines

--- tests/unit/tst_39.py
+++ tests/unit/tst_39.py
@@ -4,14 +4,11 @@ import os
 import run_tests
 from mmtbx.pair_interaction import tst_01
 
-
 def run(prefix):
   """
   Exercise standard test from pair_interactions.
   """
   tst_01.run()
-
-
 
 if(__name__ == "__main__"):
   prefix = os.path.basename(__file__).replace(".py","")
