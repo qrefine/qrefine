1e833075124e90f0442f08c4e8a2acc980abd385
Pavel <pafonine@lbl.gov>
2023-11-16 19:22:09 +1100

    do not do norm

--- restraints.py
+++ restraints.py
@@ -50,7 +50,7 @@ class restraints(object):
         restraint_objects = self.cif_objects,
         crystal_symmetry  = crystal_symmetry,
         log               = null_out())
-      model.process(make_restraints=True, grm_normalization=True,
+      model.process(make_restraints=True, grm_normalization=False,
         pdb_interpretation_params = self.pi_params)
       self.restraints_manager = from_cctbx(
         restraints_manager = model.get_restraints_manager())
