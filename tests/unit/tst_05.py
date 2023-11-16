f1edabae7dadd11ddf232200de066fa0e2b253ff
Pavel <pafonine@lbl.gov>
2023-11-16 14:21:44 +1100

    Fixed now (look differently for expected output)

--- tests/unit/tst_05.py
+++ tests/unit/tst_05.py
@@ -11,7 +11,7 @@ def run(prefix):
   xrs_good,xrs_poor,f_obs,r_free_flags = run_tests.setup_helix_example()
   r = run_tests.run_cmd(prefix,
                     args = ["restraints=cctbx","max_atoms=3"])
-  assert r.stdout_lines == ['Sorry: Too many atoms.']
+  assert 'Sorry: Too many atoms.' in r.stdout_lines
 
 if(__name__ == "__main__"):
   prefix = os.path.basename(__file__).replace(".py","")
