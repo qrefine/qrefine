9a9bdf38bd19295b9d195b5aa1e9687013e8791c
Pavel <pafonine@lbl.gov>
2023-11-16 14:48:27 +1100

    Deprecate Java version (use fast intecations only)

--- tests/unit/tst_09.py
+++ tests/unit/tst_09.py
@@ -22,24 +22,13 @@ def normalize(x):
   r.sort()
   return r
 
-def run(prefix, fast_interaction=True):
+def run(prefix):
   """
   Exercise buffer region of cluster.
   """
   #
   pdb_inp = iotbx.pdb.input(file_name= os.path.join(qr_unit_tests,"data_files","2lvr.pdb"))
   ph = pdb_inp.construct_hierarchy()
-  pyoink = None
-  if(not fast_interaction):
-    from qrefine.utils import yoink_utils
-    from qrefine.plugin.yoink.pyoink import PYoink
-    yoink_utils.write_yoink_infiles("cluster.xml",
-                                  "qmmm.xml",
-                                  ph,
-                                  os.path.join(qrefine,"plugin","yoink","dat"))
-    pyoink = PYoink(os.path.join(qrefine,"plugin","yoink","Yoink-0.0.1.jar"),
-                               os.path.join(qrefine,"plugin","yoink","dat"),
-                               "qmmm.xml")
   # Clusters
   bc_clusters = [[1, 2],
                   [3, 4, 5, 13, 14, 15, 16, 17, 18, 19, 20, 21],
@@ -53,21 +42,14 @@ def run(prefix, fast_interaction=True):
             [20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]]
 
 
-  check_buffer(ph.deep_copy(), bc_clusters, bc_qms, pyoink=pyoink,
-    fast_interaction=fast_interaction)
+  check_buffer(ph.deep_copy(), bc_clusters, bc_qms)
 
-def check_buffer(ph, clusters, qms, pyoink, fast_interaction):
+def check_buffer(ph, clusters, qms):
   #
   qms_calculated = []
   for i in range(len(clusters)):
-    if(fast_interaction):
-      fragment = pair_interaction.run(ph.deep_copy(), core=clusters[i])[2]
-      qms_calculated.append(fragment)
-    else:
-      assert pyoink is not None
-      pyoink.update(clusters[i])
-      qm_atoms, qm_molecules = pyoink.get_qm_indices()
-      qms_calculated.append(list(qm_molecules))
+    fragment = pair_interaction.run(ph.deep_copy(), core=clusters[i])[2]
+    qms_calculated.append(fragment)
   qms = normalize(qms)
   qms_calculated = normalize(qms_calculated)
   assert approx_equal(qms, qms_calculated)
