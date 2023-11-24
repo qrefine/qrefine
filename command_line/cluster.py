2225f63d722713ae98df3aa137ea692d590c61a5
Mark Waller <mpwaller@users.noreply.github.com>
2023-11-25 10:59:18 +1100

    Update cluster.py

--- command_line/cluster.py
+++ command_line/cluster.py
@@ -1,4 +1,4 @@
-# LIBTBX_SET_DISPATCHER_NAME qr.cluster_manager
+# LIBTBX_SET_DISPATCHER_NAME qr.cluster
 from __future__ import absolute_import, division, print_function
 
 from iotbx.cli_parser import run_program
