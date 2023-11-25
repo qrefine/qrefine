051655cd57a321768deb3cd9825554208ecc1998
Mark Waller <mpwaller@users.noreply.github.com>
2023-11-25 16:45:39 +1100

    Delete command_line/fetch.py
    not needed

--- command_line/fetch.py
+++
@@ -1,8 +0,0 @@
-# LIBTBX_SET_DISPATCHER_NAME qr.fetch
-from __future__ import absolute_import, division, print_function
-
-from iotbx.cli_parser import run_program
-from qrefine import fetch
-
-if __name__ == '__main__':
-  run_program(program_class=fetch.Program)
