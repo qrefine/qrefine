736a41c396edaff85edb03b27e9fae1886514386
Holger Kruse <mail2holger@gmail.com>
2024-03-04 16:35:03 +0100

    update

--- config/phenix_install.sh
+++ config/phenix_install.sh
@@ -2,6 +2,8 @@
 
 export PHENIX=`pwd`
 
+#ToDo: parse cmd line arguments to switch on/off aimnet and set phenix dir
+
 echo "####################################"
 echo "#### QREFINE INSTALLER FOR PHENIX ##"
 echo "####################################"
@@ -18,6 +20,8 @@ cd qrefine
 # update phenix conda_base
 echo "Updating Phenix's conda-base"
 conda env update -p $PHENIX/conda_base -f config/phenix.yaml
+conda env update -p $PHENIX/conda_base -f config/aimnet2.yaml
+conda clean --all
 
 ### set up build dir and exes
 echo "updating phenix/cctbx"
