69f59a22990f79002e4ba7d33d7d5f7a67bcc393
Holger Kruse <mail2holger@gmail.com>
2024-03-05 20:57:41 +1100

    update

--- config/phenix_install.sh
+++ config/phenix_install.sh
@@ -3,6 +3,22 @@
 export PHENIX=`pwd`
 
 #ToDo: parse cmd line arguments to switch on/off aimnet and set phenix dir
+# Call getopt to validate the provided input. 
+options=$(getopt --long pytorch: -- "$@")
+[ $? -eq 0 ] || { 
+    echo "Incorrect options provided"
+    exit 1
+}
+eval set -- "$options"
+while true; do
+    case "$1" in
+    --pytorch)
+        TORCH=true
+    esac
+    shift
+done
+
+
 
 echo "####################################"
 echo "#### QREFINE INSTALLER FOR PHENIX ##"
@@ -10,17 +26,12 @@ echo "####################################"
 echo ""
 echo "Phenix location:  $PHENIX"
 
-# add missing modules
-echo "Downloading qrefine"
-mkdir $PACKAGES/modules
-cd $PACKAGES/modules
-git clone https://github.com/qrefine/qrefine
-cd qrefine
-
 # update phenix conda_base
 echo "Updating Phenix's conda-base"
-conda env update -p $PHENIX/conda_base -f config/phenix.yaml
-conda env update -p $PHENIX/conda_base -f config/aimnet2.yaml
+conda env update -p $PHENIX/conda_base -f config/phenix.yaml > conda_update1.out
+if [ TORCH ]; then
+    conda env update -p $PHENIX/conda_base -f config/aimnet2.yaml > conda_update1.out
+fi
 conda clean --all
 
 ### set up build dir and exes
