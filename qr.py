1d569fed6598309ab45854b09d542a395731d26b
Pavel <pafonine@lbl.gov>
2024-04-10 20:38:39 -0700

    bugfix

--- qr.py
+++ qr.py
@@ -184,7 +184,7 @@ refine {
     .type = float
     .help = Max angle RMSD to accept optimized weight
   stop_one_found_first_good_weight = True
-    .type = float
+    .type = bool
     .help = Set True for low-res (or if use regularization), and False for high-res
   max_r_work_r_free_gap = 5.0
     .type = float
