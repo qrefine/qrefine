628ef2d1e4bf5a2ed27c916d53e8eae9b5c000d3
Pavel Afonine <pafonine@lbl.gov>
2025-04-29 12:36:20 -0700

    Remove useless assertion

--- tests/unit/tst_00.py
+++ tests/unit/tst_00.py
@@ -36,7 +36,6 @@ def run(prefix):
     xray_structure = xrs_refined)
   fmodel.update_all_scales()
   assert fmodel.r_work() < 0.04
-  assert abs(r_final-fmodel.r_work())<0.002, abs(r_final-fmodel.r_work())
 
 if(__name__ == "__main__"):
   prefix = os.path.basename(__file__).replace(".py","")
