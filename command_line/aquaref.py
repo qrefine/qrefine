4b7779810935a5e260569ba85ca457ffb223e479
Pavel <pafonine@lbl.gov>
2025-06-28 20:21:37 -0500

    add alias

--- command_line/aquaref.py
+++ command_line/aquaref.py
@@ -1,4 +1,5 @@
 # LIBTBX_SET_DISPATCHER_NAME qr.aquaref
+# LIBTBX_SET_DISPATCHER_NAME phenix.aquaref
 from __future__ import absolute_import, division, print_function
 
 from iotbx.cli_parser import run_program
